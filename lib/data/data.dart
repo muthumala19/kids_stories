b882717d7028e648c97e235314f2f4e30e3e19df
Pasindu Muthumala <lgpmuthumala@gmail.com>
2023-08-21 18:53:45 +0530

    Add images for category screen.

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -3,19 +3,84 @@ import 'package:kids_stories/models/category_model.dart';
 import 'package:kids_stories/models/story_model.dart';
 
 const categories = [
-  Category(id: 'c1', title: 'Fairy Tales', color: Color(0xFFE57373)),
-  Category(id: 'c2', title: 'Fantasy', color: Color(0xFF9575CD)),
-  Category(id: 'c3', title: 'Adventure', color: Color(0xFF4DB6AC)),
-  Category(id: 'c4', title: 'Animal Stories', color: Color(0xFFF06292)),
-  Category(id: 'c5', title: 'Folktales', color: Color(0xFFFFA726)),
-  Category(id: 'c6', title: 'Science Fiction', color: Color(0xFF64B5F6)),
-  Category(id: 'c7', title: 'Mystery', color: Color(0xFF546E7A)),
-  Category(id: 'c8', title: 'Humor', color: Color(0xFF81C784)),
-  Category(id: 'c9', title: 'Friendship', color: Color(0xFFFFD700)),
-  Category(id: 'c10', title: 'Historical Fiction', color: Color(0xFF8D6E63)),
-  Category(id: 'c11', title: 'Picture Books', color: Color(0xFFFFC107)),
-  Category(id: 'c12', title: 'Educational', color: Color(0xFF4CAF50)),
-  Category(id: 'c13', title: 'Bedtime Stories', color: Color(0xFF7986CB)),
+  Category(
+    id: 'c1',
+    title: 'Fairy Tales',
+    color: Color(0xFFE57373),
+    imagePath: 'assets/category_images/c1.png',
+  ),
+  Category(
+    imagePath: 'assets/category_images/c2.png',
+    id: 'c2',
+    title: 'Fantasy',
+    color: Color(0xFF9575CD),
+  ),
+  Category(
+    imagePath: 'assets/category_images/c3.png',
+    id: 'c3',
+    title: 'Adventure',
+    color: Color(0xFF4DB6AC),
+  ),
+  Category(
+    imagePath: 'assets/category_images/c4.png',
+    id: 'c4',
+    title: 'Animal Stories',
+    color: Color(0xFFF06292),
+  ),
+  Category(
+    imagePath: 'assets/category_images/c5.png',
+    id: 'c5',
+    title: 'Folktales',
+    color: Color(0xFFFFA726),
+  ),
+  Category(
+    imagePath: 'assets/category_images/c6.png',
+    id: 'c6',
+    title: 'Science Fiction',
+    color: Color(0xFF64B5F6),
+  ),
+  Category(
+    imagePath: 'assets/category_images/c7.png',
+    id: 'c7',
+    title: 'Mystery',
+    color: Color(0xFF546E7A),
+  ),
+  Category(
+    id: 'c8',
+    imagePath: 'assets/category_images/c8.png',
+    title: 'Humor',
+    color: Color(0xFF81C784),
+  ),
+  Category(
+    id: 'c9',
+    imagePath: 'assets/category_images/c9.png',
+    title: 'Friendship',
+    color: Color(0xFFFFD700),
+  ),
+  Category(
+    id: 'c10',
+    imagePath: 'assets/category_images/c10.png',
+    title: 'Historical Fiction',
+    color: Color(0xFF8D6E63),
+  ),
+  Category(
+    id: 'c11',
+    imagePath: 'assets/category_images/c11.png',
+    title: 'Picture Books',
+    color: Color(0xFFFFC107),
+  ),
+  Category(
+    imagePath: 'assets/category_images/c12.png',
+    id: 'c12',
+    title: 'Educational',
+    color: Color(0xFF4CAF50),
+  ),
+  Category(
+    id: 'c13',
+    imagePath: 'assets/category_images/c13.png',
+    title: 'Bedtime Stories',
+    color: Color(0xFF7986CB),
+  ),
 ];
 
 const List<Story> stories = [
@@ -191,7 +256,8 @@ const List<Story> stories = [
   Story(
     id: '9',
     title: "Zara and the Enchanted Forest",
-    imageUrl: 'https://res.cloudinary.com/dgvv3j3st/image/upload/v1692499599/kids_stories_app/Zara%20and%20the%20Enchanted%20Forest/rayqzeycbujunoqchea6.png',
+    imageUrl:
+        'https://res.cloudinary.com/dgvv3j3st/image/upload/v1692499599/kids_stories_app/Zara%20and%20the%20Enchanted%20Forest/rayqzeycbujunoqchea6.png',
     categories: ['c2', 'c9'],
     // 'Fantasy', 'Friendship'
     complexity: Complexity.simple,
--- lib/widgets/category_item_widget.dart
+++ lib/widgets/category_item_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:kids_stories/data/data.dart';
 import 'package:kids_stories/models/category_model.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 import '../screens/story_list_for_category_screen.dart';
 
@@ -11,49 +11,81 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        final selectedStories = stories.where(
-          (element) {
-            return element.categories.contains(category.id);
-          },
-        ).toList();
-        // for(){};
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (ctx) => StoriesScreen(
-              backgroundColor: category.color.withOpacity(0.3),
-              appBarTitle: category.title,
-              list: selectedStories,
-              showAppBar: true,
+    double titleFontSize = 15;
+    double cardMargin = 10;
+    double cardBorderRadius = 10;
+    double cardElevation = 2;
+    int maxLinesOfTitle = 1;
+    return Card(
+      elevation: cardElevation,
+      clipBehavior: Clip.hardEdge,
+      margin: EdgeInsets.all(cardMargin),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(cardBorderRadius),
+      ),
+      child: InkWell(
+        onTap: () {
+          final selectedStories = stories.where(
+            (element) {
+              return element.categories.contains(category.id);
+            },
+          ).toList();
+          // for(){};
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => StoriesScreen(
+                backgroundColor: category.color.withOpacity(0.3),
+                appBarTitle: category.title,
+                list: selectedStories,
+                showAppBar: true,
+              ),
             ),
-          ),
-        );
-      },
-      splashColor: Theme.of(context).primaryColor,
-      borderRadius: BorderRadius.circular(10),
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.55),
-              category.color.withOpacity(0.9),
-              category.color.withOpacity(0.9),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        alignment: Alignment.centerLeft,
-        child: Text(
-          category.title,
-          style: GoogleFonts.aBeeZee(
-            textStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.bold),
-          ),
+          );
+        },
+        child: Stack(
+          children: [
+            AspectRatio(
+              aspectRatio: 2 / 2,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: AssetImage(category.imagePath),
+                fit: BoxFit.fitWidth,
+                width: double.infinity,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                color: Colors.black45,
+                width: double.infinity,
+                padding: const EdgeInsets.all(5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          category.title,
+                          maxLines: maxLinesOfTitle,
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.visible,
+                          // to long texts add "..."
+
+                          style: TextStyle(
+                              fontSize: titleFontSize,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
