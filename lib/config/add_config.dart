36191541fbf1434c77bf0b1db20a423ae7d49235
Pasindu Muthumala <lgpmuthumala@gmail.com>
2023-09-23 10:54:20 +0530

    Add configuration file for ads

---
+++ lib/config/add_config.dart
@@ -0,0 +1,8 @@
+class AdConfigurations {
+  final String testBannerAdUnitId = 'ca-app-pub-3940256099942544/6300978111';
+  final String bannerAdUnitId = 'ca-app-pub-2468178286214637/9083020857';
+
+  final String testInterstitialAdUnitId =
+      'ca-app-pub-3940256099942544/1033173712';
+  final String interstitialAdUnitId = 'ca-app-pub-2468178286214637/3967259692';
+}
